be4a10b725aea984cc83fdfb0b099c09590f5b6d
Erin Walker <erinb0317@hotmail.com>
2018-02-04 14:46:37 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
+   
+end
